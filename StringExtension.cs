ac646093355c3e435aa25abcb563d34b0c2f1e67
obitastico <obit-obit@web.de>
2023-04-11 14:47:03 +0200

    Added Extensions For Easier Code

---
+++ StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ZipToPdfConverter
+{
+    public static class StringExtension
+    {
+        public static string RemoveRightToChar(this string str, string chr)
+        {
+            int charIndex = str.IndexOf(chr, StringComparison.Ordinal);
+            if (charIndex >= 0)
+                return str.Substring(0, charIndex);
+
+            return str;
+        }
+    }
+}
