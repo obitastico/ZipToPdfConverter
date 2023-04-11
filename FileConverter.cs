03054842a43f3929392925a839a60474af99456f
obitastico <obit-obit@web.de>
2023-04-11 14:26:23 +0200

    Works!

--- FileConverter.cs
+++ FileConverter.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using Microsoft.Office.Core;
 using Word = Microsoft.Office.Interop.Word;
 using PowerPoint = Microsoft.Office.Interop.PowerPoint;
@@ -8,32 +11,52 @@ namespace ZipToPdfConverter
 {
     public class FileConverter
     {
-        private Word.Application WordApplication { get; set; }
-        private PowerPoint.Application PowerPointApplication { get; set; }
+        private Word.Application WordApplication { get; }
+        private PowerPoint.Application PowerPointApplication { get; }
+        
+        private readonly List<string> _wordFileTypes;
+        private readonly List<string> _powerPointFileTypes;
 
         public FileConverter()
         {
             WordApplication = new Word.Application { Visible = false };
-            PowerPointApplication = new PowerPoint.Application { Visible = MsoTriState.msoFalse, };
+            PowerPointApplication = new PowerPoint.Application();
+            _wordFileTypes = new List<string> { ".docx", "doc" };
+            _powerPointFileTypes = new List<string> { ".pptx", ".ppt" };
         }
         
         public void ConvertZipToPdf(string docPath)
         {
-            string outputDir = Path.GetFileNameWithoutExtension(docPath);
-            if (!Directory.Exists(outputDir))
-                Directory.CreateDirectory(outputDir);
-            
-            var tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
             Directory.CreateDirectory(tempDirectory);
 
-            using (var archive = ZipArchive)
+            ZipFile.ExtractToDirectory(docPath, tempDirectory);
+
+            foreach (string filePath in Directory.GetFiles(tempDirectory))
             {
-                archive.ExtractToDirectory(tempDirectory);
+                if (!_wordFileTypes.Concat(_powerPointFileTypes).Contains(Path.GetExtension(filePath)))
+                    continue;
+                
+                if (_wordFileTypes.Contains(Path.GetExtension(filePath)))
+                    ConvertWordToPdf(filePath, filePath.Replace(Path.GetExtension(filePath), ".pdf"));
+                else
+                    ConvertPowerPointToPdf(filePath, filePath.Replace(Path.GetExtension(filePath), ".pdf"));
+
+                File.Delete(filePath);
             }
 
+            int dashIndex = docPath.IndexOf("-", StringComparison.Ordinal);
+
+            string outputDir = docPath;
+            if (dashIndex >= 0)
+                outputDir = docPath.Substring(0, dashIndex) + ".zip";
+            
+            if (File.Exists(outputDir))
+                File.Delete(outputDir);
+            
+            ZipFile.CreateFromDirectory(tempDirectory, outputDir);
 
             Directory.Delete(tempDirectory, true);
-            
         }
         
         private void ConvertWordToPdf(string docPath, string outPath)
@@ -47,11 +70,11 @@ namespace ZipToPdfConverter
 
         private void ConvertPowerPointToPdf(string docPath, string outPath)
         {
-            PowerPointApplication.Presentations.Open(docPath, MsoTriState.msoTrue, MsoTriState.msoFalse, MsoTriState.msoFalse);
+            PowerPoint.Presentation presentation = PowerPointApplication.Presentations.Open(docPath, MsoTriState.msoTrue, MsoTriState.msoFalse, MsoTriState.msoFalse);
             
-            PowerPointApplication.ActivePresentation.ExportAsFixedFormat(outPath, PowerPoint.PpFixedFormatType.ppFixedFormatTypePDF);
+            presentation.ExportAsFixedFormat(outPath, PowerPoint.PpFixedFormatType.ppFixedFormatTypePDF);
 
-            PowerPointApplication.ActivePresentation.Close();
+            presentation.Close();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.IO;
-
-namespace ZipToPdfConverter
+﻿namespace ZipToPdfConverter
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            string docPath = args.Length > 0 ? args[0] : @"D:\Downloads\4 Weimars Krisenjahre-20230322.zip";
+            string docPath = args.Length > 0 ? args[0] : @"D:\Downloads\1 Der Versailler Vertrag-20230301.zip";
             FileConverter fileConverter = new FileConverter();
             fileConverter.ConvertZipToPdf(docPath);
         }
